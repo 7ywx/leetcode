580e2160222f587713c1cff48e8e2d7b455e008b
ywx <2819142473@qq.com>
2023-12-23 00:05:48 +0800

    185

---
+++ .history/185.部门工资前三高的所有员工_20231222231419.py
@@ -0,0 +1,24 @@
+import pandas as pd
+def top_three_salaries(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    e_d = Employee.merge(Department, left_on='departmentId', right_on='id', how='inner').rename(columns={'name_y': 'Department', 'name_x': 'Employee', 'salary': 'Salary'})
+
+    print(e_d)
+
+
+
+
+
+# Create the Employee table
+Employee = pd.DataFrame({
+    'id': [1, 2, 3, 4, 5, 6, 7],
+    'name': ['Joe', 'Henry', 'Sam', 'Max', 'Janet', 'Randy', 'Will'],
+    'salary': [85000, 80000, 60000, 90000, 69000, 85000, 70000],
+    'departmentId': [1, 2, 2, 1, 1, 1, 1]
+})
+
+# Create the Department table
+Department = pd.DataFrame({
+    'id': [1, 2],
+    'name': ['IT', 'Sales']
+})
+top_three_salaries(Employee, Department)
