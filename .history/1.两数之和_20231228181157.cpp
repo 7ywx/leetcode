237a682fd46ea8bc7557684d67d55e54c9e1da45
7ywx <y28192473@163.com>
2023-12-28 18:51:43 +0800

    1，添加了哈希表的解法

--- .history/1.两数之和_20231228181157.cpp
+++ .history/1.两数之和_20231228181157.cpp
@@ -11,41 +11,33 @@ using namespace std;
 class Solution
 {
 public:
-    // vector<int> twoSum(vector<int> &nums, int target)
-    // {
-    //     vector<int> two;
-    //     for (int i = 0; i < nums.size(); i++)
-    //     {
-    //         for (int j = i + 1; j < nums.size(); j++)
-    //         {
-    //             if (nums[i] + nums[j] == target)
-    //             {
-    //                 return {i, j};
-    //             }
-    //         }
-    //     }
-    //     return {};
-    // }
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        // 哈希表，用于存储目标值与元素下标的对应关系
+        vector<int> two;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         unordered_map<int, int> hashtable;
-
-        // 遍历数组
         for (int i = 0; i < nums.size(); ++i)
         {
-            // 在哈希表中查找目标值与当前元素的差值
             auto it = hashtable.find(target - nums[i]);
             if (it != hashtable.end())
             {
-                // 如果找到匹配的差值，则返回对应的两个元素下标
                 return {it->second, i};
             }
-            // 将当前元素及其下标存入哈希表
             hashtable[nums[i]] = i;
         }
-
-        // 如果未找到匹配的差值，则返回空向量
         return {};
     }
 };
--- .history/1.两数之和_20231228181208.cpp
+++ .history/1.两数之和_20231228181208.cpp
@@ -28,24 +28,16 @@ public:
     // }
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        // 哈希表，用于存储目标值与元素下标的对应关系
         unordered_map<int, int> hashtable;
-
-        // 遍历数组
         for (int i = 0; i < nums.size(); ++i)
         {
-            // 在哈希表中查找目标值与当前元素的差值
             auto it = hashtable.find(target - nums[i]);
             if (it != hashtable.end())
             {
-                // 如果找到匹配的差值，则返回对应的两个元素下标
                 return {it->second, i};
             }
-            // 将当前元素及其下标存入哈希表
             hashtable[nums[i]] = i;
         }
-
-        // 如果未找到匹配的差值，则返回空向量
         return {};
     }
 };
