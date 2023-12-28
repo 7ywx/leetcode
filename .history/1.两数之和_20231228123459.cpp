0665ce9f4448bb3e579b341baacd3ff8aa1a1ffd
7ywx <y28192473@163.com>
2023-12-28 13:00:19 +0800

    1

---
+++ .history/1.两数之和_20231228123459.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    vector<int> two = {nums[i], nums[j]};
+                    return two;
+                }
+            }
+        }
+    }
+};
+int main(){
+
+}
+// @lc code=end
---
+++ 1721.交换链表中的节点.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=1721 lang=python3
+#
+# [1721] 交换链表中的节点
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+# @lc code=end
+
