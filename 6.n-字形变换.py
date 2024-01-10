c84d081aa01def5f428dd8186512009f889d40b0
ywx <2819142473@qq.com>
2024-01-10 21:14:14 +0800

    6添加了最优解和chatgpt解

--- 6.n-字形变换.py
+++ 6.n-字形变换.py
@@ -71,24 +71,72 @@
 # @lc code=start
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:
+        #chatgpt
+        # # 如果只有一行或行数大于等于字符串长度，直接返回原字符串
+        # if numRows == 1 or numRows >= len(s):
+        #     return s
+
+        # # 创建一个列表，用于存储每一行的字符串
+        # result = [''] * numRows
+        # print(result)
+        # index, step = 0, 1  # index表示当前行的索引，step表示行索引的变化方向
+
+        # # 遍历原始字符串
+        # for char in s:
+        #     # 将字符添加到相应行的字符串中
+        #     result[index] += char
+        #     # 根据Z字形变换的规律更新行索引和方向
+        #     if index == 0:
+        #         step = 1  # 当索引到达最顶部时，行索引向下移动
+        #     elif index == numRows - 1:
+        #         step = -1  # 当索引到达最底部时，行索引向上移动
+        #     index += step
+
+        # # 将每一行的字符串连接起来得到最终结果
+        # print(result)
+        # return ''.join(result)
+
+
+        # 如果给定字符串的行数小于2，则直接返回该字符串
+        if numRows < 2:
             return s
-        else:
-            rowNum = ((len(s) // (2 * numRows - 2)) + 1) * numRows
-            s2d = [[''] * rowNum for _ in range(numRows)]
-            for i in range(len(s)):
-                row = i // (2 * numRows - 2)
-                if i < row * (2 * numRows - 2) + numRows:
-                    s2d[i-(row * (2*numRows - 2))][row * (numRows - 1)] = s[i]
-                else:
-                    s2d[numRows-1-(i-(numRows -1 + row*(2*numRows-2)))][i-(numRows -1 + row*(2*numRows-2))+row * (numRows - 1)] = s[i]
-            result = []
-            for row in s2d:
-                for element in row:
-                    if element != '':
-                        result.append(element)
-            # print(''.join(result))
-            return ''.join(result)
+        # 创建一个空列表，行数与给定字符串的行数相同
+        res = ["" for _ in range(numRows)]
+        # 初始化索引值i为0
+        i = 0
+        # 初始化标志位flag为-1（+1：向下移动，-1：向上移动）
+        flag = -1
+        # 遍历给定字符串的每个字符
+        for c in s:
+            # 将字符c添加到res列表的第i行
+            res[i] += c
+            # 如果i等于0或numRows-1（即将要遍历最后一行），则将标志位flag取反
+            if i == 0 or i == numRows-1:
+                flag = -flag
+            # 将索引值i更新为下一个位置
+            i += flag
+        # 将res列表中的字符串连接成一个字符串，并返回
+        return "".join(res)
+
+    # def convert(self, s: str, numRows: int) -> str:
+    #     if numRows == 1:
+    #         return s
+    #     else:
+    #         rowNum = ((len(s) // (2 * numRows - 2)) + 1) * numRows
+    #         s2d = [[''] * rowNum for _ in range(numRows)]
+    #         for i in range(len(s)):
+    #             row = i // (2 * numRows - 2)
+    #             if i < row * (2 * numRows - 2) + numRows:
+    #                 s2d[i-(row * (2*numRows - 2))][row * (numRows - 1)] = s[i]
+    #             else:
+    #                 s2d[numRows-1-(i-(numRows -1 + row*(2*numRows-2)))][i-(numRows -1 + row*(2*numRows-2))+row * (numRows - 1)] = s[i]
+    #         result = []
+    #         for row in s2d:
+    #             for element in row:
+    #                 if element != '':
+    #                     result.append(element)
+    #         # print(''.join(result))
+    #         return ''.join(result)
 # @lc code=end
 solution = Solution()
 solution.convert("PAYPALISHIRING", 3)
