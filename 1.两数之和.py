de09d8a1cb9ed3ca5772e45ed61729a0c37fa41b
ywx <2819142473@qq.com>
2024-01-20 16:08:12 +0800

    1添加了注释

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -11,10 +11,9 @@ class Solution:
         hashtable = {}
         for index, num in enumerate(nums):
             if target - num in hashtable: # 在 if a in hashtable: 这样的条件判断中，Python会检查字典（哈希表）hashtable 的键（keys）中是否包含变量 a。也就是说，它不会查找值（values），而是查找键。
-                return [hashtable[target - num], index]
+                return [hashtable[target - num], index] # 如果存在，返回对应的下标。
             hashtable[num] = index # 将当前元素及其下标存入哈希表
-        print(hashtable)
-        return []
+        return [] # 如果不存在，返回空列表。
 # @lc code=end
 solution = Solution()
 solution.twoSum(nums=[2, 7, 11, 15], target=9)
