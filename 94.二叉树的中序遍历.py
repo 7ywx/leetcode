b86eded4ecc26da9e4dcb8063dd7cbfccf595375
Ywx_workplace <y2819142473@163.com>
2024-02-28 21:26:30 +0800

    94递归解法

---
+++ 94.二叉树的中序遍历.py
@@ -0,0 +1,83 @@
+#
+# @lc app=leetcode.cn id=94 lang=python3
+#
+# [94] 二叉树的中序遍历
+#
+# https://leetcode.cn/problems/binary-tree-inorder-traversal/description/
+#
+# algorithms
+# Easy (76.58%)
+# Likes:    2043
+# Dislikes: 0
+# Total Accepted:    1.4M
+# Total Submissions: 1.8M
+# Testcase Example:  '[1,null,2,3]'
+#
+# 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：root = [1,null,2,3]
+# 输出：[1,3,2]
+#
+#
+# 示例 2：
+#
+#
+# 输入：root = []
+# 输出：[]
+#
+#
+# 示例 3：
+#
+#
+# 输入：root = [1]
+# 输出：[1]
+#
+#
+#
+#
+# 提示：
+#
+#
+# 树中节点数目在范围 [0, 100] 内
+# -100 <= Node.val <= 100
+#
+#
+#
+#
+# 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+#
+#
+from typing import List, Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+
+        # 递归
+        res = []
+        if not root:
+            return res
+        res.extend(self.inorderTraversal(root.left))
+        res.append(root.val)
+        res.extend(self.inorderTraversal(root.right))
+        return res
+# @lc code=end
+solution = Solution()
+root = TreeNode(val=1, left=None, right=TreeNode(val=2,left=TreeNode(3),right=None))
+print(solution.inorderTraversal(root))
