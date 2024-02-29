a69df92857348c2c9df9609e9a24773c55a714ac
Ywx_workplace <y2819142473@163.com>
2024-02-29 20:17:41 +0800

    145迭代

--- 145.二叉树的后序遍历.py
+++ 145.二叉树的后序遍历.py
@@ -52,7 +52,12 @@
 # 进阶：递归算法很简单，你可以通过迭代算法完成吗？
 #
 #
-
+from typing import List, Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 # @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
@@ -62,7 +67,60 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        #标签 迭代
         if not root:
             return []
-        return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+        stack = [root]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        print(res)
+        return res[::-1]
+
+        # # 递归
+        # if not root:
+        #     return []
+        # return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
 # @lc code=end
+def build_tree(nodes):
+    if not nodes:
+        return None
+
+    root = TreeNode(nodes[0])
+    queue = [root]
+    i = 1
+
+    while queue and i < len(nodes):
+        current_node = queue.pop(0)
+
+        if nodes[i] is not None:
+            current_node.left = TreeNode(nodes[i])
+            queue.append(current_node.left)
+
+        i += 1
+
+        if i < len(nodes) and nodes[i] is not None:
+            current_node.right = TreeNode(nodes[i])
+            queue.append(current_node.right)
+
+        i += 1
+
+    return root
+
+# 用给定的节点列表构建二叉树
+nodes_list = [1, 2, 3, 4, 5]
+root = build_tree(nodes_list)
+
+# 给定的前序遍历数组
+preorder_traversal = [1, 2, 3, 4, 5]
+
+# 构建二叉树
+root = build_tree(preorder_traversal)
+
+solution = Solution()
+print(solution.postorderTraversal(root))
