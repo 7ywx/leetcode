f86df280864aa9ea7514e81fb3e4281922cef18c
Ywx_workplace <y2819142473@163.com>
2024-02-29 19:20:04 +0800

    145递归

--- 145.二叉树的后序遍历.py
+++ 145.二叉树的后序遍历.py
@@ -64,5 +64,6 @@ class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
+
         return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
 # @lc code=end
