5449824f4fb8f6e5ef67c8eab1af3604a30b6efe
Ywx_workplace <y2819142473@163.com>
2024-03-01 17:03:23 +0800

    102添加了官方题解及算法流程

--- 102.二叉树的层序遍历.py
+++ 102.二叉树的层序遍历.py
@@ -63,29 +63,59 @@ class TreeNode:
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """
+        算法流程：
+            1. 特例处理： 当根节点为空，则返回空列表 [] 。
+            2. 初始化： 打印结果列表 res = [] ，包含根节点的队列 queue = [root] 。
+            3. BFS 循环： 当队列 queue 为空时跳出。
+                a. 新建一个临时列表 tmp ，用于存储当前层打印结果。
+                b. 当前层打印循环： 循环次数为当前层节点数（即队列 queue 长度）。
+                    a. 出队： 队首元素出队，记为 node。
+                    b. 打印： 将 node.val 添加至 tmp 尾部。
+                    c. 添加子节点： 若 node 的左（右）子节点不为空，则将左（右）子节点加入队列 queue 。
+                c. 将当前层结果 tmp 添加入 res 。
+            4. 返回值： 返回打印结果列表 res 即可。
+        """
         if not root:
             return []
-        stack = []
         res = []
-        depth = 1
-        while root or stack:
-            if not res:
-                res.append([root.val])
-            else:
-                if depth != root_depth[1]:
-                    res.append([])
-                    depth = root_depth[1]
-                res[-1].append(root.val)
-            if root.left:
-                stack.append((root.left, depth + 1))
-            if root.right:
-                stack.append((root.right, depth + 1))
-            if stack:
-                root_depth = stack.pop(0)
-                root = root_depth[0]
-            else:
-                root = None
+        queue = collections.deque() # double-ended queue
+        queue.append(root)
+        while queue:
+            temp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(temp)
         return res
+
+        # if not root:
+        #     return []
+        # stack = []
+        # res = []
+        # depth = 1
+        # while root or stack:
+        #     if not res:
+        #         res.append([root.val])
+        #     else:
+        #         if depth != root_depth[1]:
+        #             res.append([])
+        #             depth = root_depth[1]
+        #         res[-1].append(root.val)
+        #     if root.left:
+        #         stack.append((root.left, depth + 1))
+        #     if root.right:
+        #         stack.append((root.right, depth + 1))
+        #     if stack:
+        #         root_depth = stack.pop(0)
+        #         root = root_depth[0]
+        #     else:
+        #         root = None
+        # return res
 # @lc code=end
 def build_tree(nodes):
     # 如果节点列表为空，则返回空树
