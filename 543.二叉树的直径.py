7940c02b5dd1b6dfdcbfef41968d407b04c4dbbb
Ywx_workplace <y2819142473@163.com>
2024-03-01 11:39:14 +0800

    543添加了一些注释和测试用例

--- 543.二叉树的直径.py
+++ 543.二叉树的直径.py
@@ -46,7 +46,12 @@
 #
 #
 #
-
+from typing import Optional, List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 # @lc code=start
 # Definition for a binary tree node.
 # class TreeNode:
@@ -54,8 +59,56 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+#         # 二叉树的直径，就是左右子树的最大深度之和
 class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # 二叉树的直径，就是左右子树的最大深度之和
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 1
+        def depth(node):
+            # 访问到空节点了，返回0
+            if not node:
+                return 0
+            # 左儿子为根的子树的深度
+            L = depth(node.left)
+            # 右儿子为根的子树的深度
+            R = depth(node.right)
+            # 计算d_node = L + R + 1 (1是那个根) 并更新ans
+            self.ans = max(self.ans, L + R + 1)
+            # 返回该节点为根的子树的深度
+            return max(L, R) + 1
 
+        depth(root)
+        return self.ans - 1
 # @lc code=end
+def build_tree(nodes):
+    if not nodes:
+        return None
+
+    root = TreeNode(nodes[0])
+    queue = [root]
+    i = 1
+
+    while queue and i < len(nodes):
+        current_node = queue.pop(0)
+
+        if nodes[i] is not None:
+            current_node.left = TreeNode(nodes[i])
+            queue.append(current_node.left)
+
+        i += 1
+
+        if i < len(nodes) and nodes[i] is not None:
+            current_node.right = TreeNode(nodes[i])
+            queue.append(current_node.right)
+
+        i += 1
+
+    return root
+
+# 用给定的节点列表构建二叉树
+nodes_list = [1, 2, 3, 4, 5]
+root = build_tree(nodes_list)
+
+solution = Solution()
+solution.diameterOfBinaryTree(root)
