5cbf5794115926b7e6a3be2605dbe079b7bed878
7ywx <y28192473@163.com>
2024-03-06 20:40:19 +0800

    200

---
+++ 200.岛屿数量.py
@@ -0,0 +1,104 @@
+#
+# @lc app=leetcode.cn id=200 lang=python3
+#
+# [200] 岛屿数量
+#
+# https://leetcode.cn/problems/number-of-islands/description/
+#
+# algorithms
+# Medium (60.02%)
+# Likes:    2431
+# Dislikes: 0
+# Total Accepted:    777.2K
+# Total Submissions: 1.3M
+# Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+#
+# 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+#
+# 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+#
+# 此外，你可以假设该网格的四条边均被水包围。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：grid = [
+# ⁠ ["1","1","1","1","0"],
+# ⁠ ["1","1","0","1","0"],
+# ⁠ ["1","1","0","0","0"],
+# ⁠ ["0","0","0","0","0"]
+# ]
+# 输出：1
+#
+#
+# 示例 2：
+#
+#
+# 输入：grid = [
+# ⁠ ["1","1","0","0","0"],
+# ⁠ ["1","1","0","0","0"],
+# ⁠ ["0","0","1","0","0"],
+# ⁠ ["0","0","0","1","1"]
+# ]
+# 输出：3
+#
+#
+#
+#
+# 提示：
+#
+#
+# m == grid.length
+# n == grid[i].length
+# 1
+# grid[i][j] 的值为 '0' 或 '1'
+#
+#
+#
+from typing import List, Optional
+def print_grid(grid: List[List[str]]) -> None:
+    for i in grid:
+        print(i)
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        #TODO 优化
+        res = 0
+        n = len(grid)
+        m = len(grid[0])
+        def infect(x, y):
+            if grid[x][y] == '1':
+                grid[x][y] = '-1'
+                if x > 0:
+                    infect(x-1, y)
+                if x < n-1:
+                    infect(x+1, y)
+                if y > 0:
+                    infect(x, y-1)
+                if y < m-1:
+                    infect(x, y+1)
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    infect(i, j)
+                    res += 1
+        # print(res)
+        return res
+# @lc code=end
+solution = Solution()
+# solution.numIslands([
+#     ["1","1","1","1","0"],
+#     ["1","1","0","1","0"],
+#     ["1","1","0","0","0"],
+#     ["0","0","0","0","0"]
+# ])
+
+solution.numIslands([
+    ["1","1","0","0","0"],
+    ["1","1","0","0","0"],
+    ["0","0","1","0","0"],
+    ["0","0","0","1","1"]
+])
