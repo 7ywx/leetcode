51fcac3b5df6e576ee9988505e8db11b6506885b
Ywx_workplace <y2819142473@163.com>
2024-03-10 20:50:39 +0800

    整理

--- 9.回文数.py
+++ 9.回文数.py
@@ -63,15 +63,18 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # v1.1 装小逼，一行的写法 40ms
         return str(x) == str(x)[::-1]
-        # if x < 0:
-        #     return False
-        # x_str = str(x)
-        # i, j= 0, len(x_str) - 1
-        # while i < j:
-        #     if x_str[i] != x_str[j]:
-        #         return False
-        #     i += 1
-        #     j -= 1
-        # return True
+
+        # # v1.0 普通写法 50ms
+        if x < 0:
+            return False
+        x_str = str(x)
+        i, j= 0, len(x_str) - 1
+        while i < j:
+            if x_str[i] != x_str[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
 # @lc code=end
