ec7b429dee971dba3ae216955170dd845cd3d87e
Ywx_workplace <y2819142473@163.com>
2024-03-21 11:41:11 +0800

    添加了一些todo

--- 8.字符串转换整数-atoi.py
+++ 8.字符串转换整数-atoi.py
@@ -99,7 +99,7 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         # 去除开头的空格
-        s = s.lstrip()
+        s = s.lstrip() #TODO python备忘录-函数
 
         if not s:
             return 0  # 字符串为空，返回0
@@ -115,7 +115,7 @@ class Solution:
         # 读取数字，直到遇到非数字字符
         num = 0
         for char in s:
-            if char.isdigit():
+            if char.isdigit(): #TODO python备忘录-函数
                 num = num * 10 + int(char)
             else:
                 break
