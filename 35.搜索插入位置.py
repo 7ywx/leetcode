3c532e8b4134399b1860b00dee47994e197f935a
7ywx <y28192473@163.com>
2024-03-23 10:11:52 +0800

    添加了注释捏

--- 35.搜索插入位置.py
+++ 35.搜索插入位置.py
@@ -56,25 +56,41 @@ from typing import List
 # @lc code=sta
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        """
+        在给定的有序整数列表中，查找目标值出现的位置。如果目标值不存在，则返回它应该被插入的位置。
+
+        参数:
+        nums: List[int] -- 一个有序的整数列表。
+        target: int -- 需要查找或插入的目标整数。
+
+        返回值:
+        int -- 目标值在列表中的索引，或者它应该被插入的位置。
+        """
         nums_length = len(nums)
-        left, right = 0, nums_length-1
+        left, right = 0, nums_length-1  # 初始化二分查找的左右边界
+
+        # 处理目标值小于列表中最小值或大于列表中最大值的情况
         if target < nums[left]:
             return 0
         if nums[right] < target:
             return nums_length
+
+        # 二分查找
         while left != right - 1:
             mid = (left + right) // 2
-            if nums[mid] < target:
+            if nums[mid] < target:  # 目标值在右半部分
                 left = mid
-            elif target < nums[mid]:
+            elif target < nums[mid]:  # 目标值在左半部分
                 right = mid
-            else:
+            else:  # 找到目标值
                 return mid
+
+        # 检查最后的边界是否为目标值
         if nums[left] == target:
             return left
         if nums[right] == target:
             return right
-        return right
+        return right  # 如果目标值不存在，返回插入位置
 # @lc code=end
 s = Solution()
 # print(s.searchInsert([1,3,5,6], 2))
