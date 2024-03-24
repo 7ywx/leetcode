e23e135e9b8d8fbb59bdf24762470bca5137f807
Ywx_workplace <y2819142473@163.com>
2024-03-24 12:36:46 +0800

    3种区间的二分查找

--- 35.搜索插入位置.py
+++ 35.搜索插入位置.py
@@ -54,6 +54,48 @@
 #
 from typing import List
 # @lc code=sta
+# 闭区间写法
+def lower_bound(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1  # 闭区间 [left, right]
+    while left <= right:  # 区间不为空
+        # 循环不变量：
+        # nums[left-1] < target
+        # nums[right+1] >= target
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1  # 范围缩小到 [mid+1, right]
+        else:
+            right = mid - 1  # 范围缩小到 [left, mid-1]
+    return left  # 或者 right+1
+
+# 左闭右开区间写法
+def lower_bound2(nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums)  # 左闭右开区间 [left, right)
+    while left < right:  # 区间不为空
+        # 循环不变量：
+        # nums[left-1] < target
+        # nums[right] >= target
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1  # 范围缩小到 [mid+1, right)
+        else:
+            right = mid  # 范围缩小到 [left, mid)
+    return left  # 或者 right
+
+# 开区间写法
+def lower_bound3(nums: List[int], target: int) -> int:
+    left, right = -1, len(nums)  # 开区间 (left, right)
+    while left + 1 < right:  # 区间不为空
+        mid = (left + right) // 2
+        # 循环不变量：
+        # nums[left] < target
+        # nums[right] >= target
+        if nums[mid] < target:
+            left = mid  # 范围缩小到 (mid, right)
+        else:
+            right = mid  # 范围缩小到 (left, mid)
+    return right  # 或者 left+1
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         """
