39da42ca89e8da787f2936b7616e302ea9b3771d
Ywx_workplace <y2819142473@163.com>
2024-03-30 21:49:14 +0800

    看了一眼提示立马写出来了，感觉跟跳跃游戏很像

---
+++ 763.划分字母区间.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode.cn id=763 lang=python3
+#
+# [763] 划分字母区间
+#
+# https://leetcode.cn/problems/partition-labels/description/
+#
+# algorithms
+# Medium (76.85%)
+# Likes:    1117
+# Dislikes: 0
+# Total Accepted:    221K
+# Total Submissions: 287.6K
+# Testcase Example:  '"ababcbacadefegdehijhklij"'
+#
+# 给你一个字符串 s 。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。
+#
+# 注意，划分结果需要满足：将所有划分结果按顺序连接，得到的字符串仍然是 s 。
+#
+# 返回一个表示每个字符串片段的长度的列表。
+#
+#
+# 示例 1：
+#
+#
+# 输入：s = "ababcbacadefegdehijhklij"
+# 输出：[9,7,8]
+# 解释：
+# 划分结果为 "ababcbaca"、"defegde"、"hijhklij" 。
+# 每个字母最多出现在一个片段中。
+# 像 "ababcbacadefegde", "hijhklij" 这样的划分是错误的，因为划分的片段数较少。
+#
+# 示例 2：
+#
+#
+# 输入：s = "eccbbbbdec"
+# 输出：[10]
+#
+#
+#
+#
+# 提示：
+#
+#
+# 1 <= s.length <= 500
+# s 仅由小写英文字母组成
+#
+#
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        #TODO yield 语法 last字典
+        last = {}
+        n = len(set(s))
+        for c in range(len(s)-1, -1, -1):
+            if s[c] not in last:
+                last[s[c]] = c
+            if len(last) == n:
+                break
+        start, end = 0, last[s[0]]
+        res = []
+        for i in range(len(s)):
+            if i == end:
+                res.append(end-start+1)
+                start = i + 1
+                if i != len(s)-1:
+                    end = last[s[i+1]]
+            else:
+                # end = max(end, last[s[i]])
+                if last[s[i]] > end:
+                    end = last[s[i]]
+        return res
+# @lc code=end
+s = Solution()
+print(s.partitionLabels("eaaaabaaec"))
+# print(s.partitionLabels("ababcbacadefegdehijhklij"))
