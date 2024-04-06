3595cd0d1e661a6f4214298b3df1e93e058e890d
Ywx_workplace <y2819142473@163.com>
2024-04-06 12:11:29 +0800

    官方题解

--- 152.乘积最大子数组.py
+++ 152.乘积最大子数组.py
@@ -49,38 +49,52 @@ from typing import List
 # @lc code=start
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        dp = [nums[0]] + [0] * (len(nums) - 1)
-        tail = []
-        if nums[0] < 0:
-            tail.append(0)
+        # 当前位置的最优解未必是由前一个位置的最优解转移得到的。
+
+        # 初始化最大乘积和最小乘积列表，起始值为列表的第一个元素
+        maxF, minF = [nums[0]], [nums[0]]
+
         for i in range(1, len(nums)):
-            if nums[i] > 0:
-                if dp[i-1] > 0:
-                    dp[i] = dp[i-1] * nums[i]
-                else:
-                    dp[i] = nums[i]
-            elif nums[i] < 0:
-                tail.append(i)
-                if len(tail) % 2 == 0:
-                    temp = 1
-                    for j in range(tail[0], tail[-1] + 1):
-                        temp *= nums[j]
-                    if tail[0] > 0 and dp[tail[0] - 1] > 0:
-                        temp *= dp[tail[0] - 1]
-                    dp[i] = temp
-                else:
-                    if len(tail) == 1:
-                        dp[i] = nums[i]
-                    else:
-                        temp = nums[i]
-                        for j in range(tail[1], tail[-1]):
-                            temp *= nums[j]
-                        if tail[1] > 0 and dp[tail[1]-1] > 0:
-                            temp *= dp[tail[1]-1]
-                        dp[i] = temp
-            else:
-                tail.clear()
-        return max(dp)
+            num = nums[i]
+            index = i
+            # 计算当前位置的最大乘积和最小乘积
+            maxF.append(max(num, maxF[index-1] * num, minF[index-1] * num))
+            minF.append(min(num, minF[index-1] * num, maxF[index-1] * num))
+        # 返回最大乘积
+        return max(maxF)
+
+        # dp = [nums[0]] + [0] * (len(nums) - 1)
+        # tail = []
+        # if nums[0] < 0:
+        #     tail.append(0)
+        # for i in range(1, len(nums)):
+        #     if nums[i] > 0:
+        #         if dp[i-1] > 0:
+        #             dp[i] = dp[i-1] * nums[i]
+        #         else:
+        #             dp[i] = nums[i]
+        #     elif nums[i] < 0:
+        #         tail.append(i)
+        #         if len(tail) % 2 == 0:
+        #             temp = 1
+        #             for j in range(tail[0], tail[-1] + 1):
+        #                 temp *= nums[j]
+        #             if tail[0] > 0 and dp[tail[0] - 1] > 0:
+        #                 temp *= dp[tail[0] - 1]
+        #             dp[i] = temp
+        #         else:
+        #             if len(tail) == 1:
+        #                 dp[i] = nums[i]
+        #             else:
+        #                 temp = nums[i]
+        #                 for j in range(tail[1], tail[-1]):
+        #                     temp *= nums[j]
+        #                 if tail[1] > 0 and dp[tail[1]-1] > 0:
+        #                     temp *= dp[tail[1]-1]
+        #                 dp[i] = temp
+        #     else:
+        #         tail.clear()
+        # return max(dp)
 
         # dp = nums[:]
         # for i in range(1, len(nums)):
@@ -95,12 +109,12 @@ class Solution:
         # return max(dp)
 # @lc code=end
 s = Solution()
-# print(s.maxProduct([2,3,-2,4]))
+print(s.maxProduct([2,3,-2,4]))
 
 # print(s.maxProduct([0,2]))
 # print(s.maxProduct([3,-1,4]))
 # print(s.maxProduct([-3,-1,-1]))
-print(s.maxProduct([7,-2,-4]))
+# print(s.maxProduct([7,-2,-4]))
 # print(s.maxProduct([-2,-3,7]))
 # print(s.maxProduct([2,-5,-2,-4,3]))
 # print(s.maxProduct([-2,0,-1]))
