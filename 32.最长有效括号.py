584145d71bb9b508b583a87893794cb637c43491
Ywx_workplace <y2819142473@163.com>
2024-04-07 12:34:33 +0800

    dp

--- 32.最长有效括号.py
+++ 32.最长有效括号.py
@@ -58,6 +58,22 @@
 # @lc code=start
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * n  # 初始化 DP 数组，存储以索引 i 结尾的最长有效括号子串的长度
+        max_length = 0  # 记录最长有效括号子串的长度
+
+        # 遍历字符串，更新 DP 数组
+        for i in range(1, n):
+            if s[i] == ')':  # 当前字符为右括号
+                if s[i-1] == '(':  # 如果前一个字符是左括号，则形成了一个基本单位 "()"
+                    dp[i] = (dp[i-2] if i >= 2 else 0) + 2
+                elif i - dp[i-1] > 0 and s[i - dp[i-1] - 1] == '(':  # 前一个字符为右括号，并且在当前匹配的左括号的前面有一个有效括号子串
+                    # dp[i-1] 表示前一个有效括号子串的长度，dp[i - dp[i-1] - 2] 表示在当前匹配的左括号的前面的有效括号子串的长度
+                    dp[i] = dp[i-1] + (dp[i - dp[i-1] - 2] if i - dp[i-1] >= 2 else 0) + 2
+                max_length = max(max_length, dp[i])  # 更新最长有效括号子串的长度
+
+        return max_length  # 返回最长有效括号子串的长度
+
         # 初始化栈和有效性数组
         stack = []
         isValid = [False] * (len(s)) # 记录每个位置是否是合法的括号
