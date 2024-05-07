b37ccbeb0aa56eb0a66abc5af098f2d5f103ca35
Ywx_workplace <y2819142473@163.com>
2024-05-07 16:59:59 +0800

    删除了一下注释

--- 55.跳跃游戏.py
+++ 55.跳跃游戏.py
@@ -70,15 +70,4 @@ class Solution:
                 jump = i + nums[i]
 
         return True
-
-        # n = len(nums)
-        # farthest = 0
-        # for i in range(n - 1):
-        #     # 不断计算能跳到的最远距离
-        #     farthest = max(farthest, i + nums[i])
-        #     # 可能碰到了 0，卡住跳不动了
-        #     if farthest <= i:
-        #         return False
-        # return farthest >= n - 1
-
 # @lc code=end
