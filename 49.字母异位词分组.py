43436a9a7d2737dc889fabd87e029f18e4fd77fa
ywx-desktop <2819142473@qq.com>
2024-09-13 13:19:24 +0800

    精简

--- 49.字母异位词分组.py
+++ 49.字母异位词分组.py
@@ -54,15 +54,6 @@ from typing import List
 from collections import defaultdict
 # @lc code=start
 class Solution:
-    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    #     hashtable = {}
-    #     for s in strs:
-    #         key = ''.join(sorted(s))
-    #         if key not in hashtable:
-    #             hashtable[key] = [s]
-    #         else:
-    #             hashtable[key].append(s)
-    #     return list(hashtable.values())
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list) # 当试图访问 anagrams 中尚未存在的键时，它将自动创建并关联一个空列表作为该键的值。
 
@@ -72,45 +63,6 @@ class Solution:
             anagrams[key].append(s)
 
         return list(anagrams.values())
-    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    #     def is_anagram(s1, s2):
-    #         if len(s1) != len(s2):
-    #             return False
-    #         char_count = {}
-    #         for char in s1:
-    #             char_count[char] = char_count.get(char, 0) + 1
-    #         for char in s2:
-    #             if char not in char_count or char_count[char] == 0:
-    #                 return False
-    #             char_count[char] -= 1
-    #         return all(count == 0 for count in char_count.values())
-    #     def is_anagram_wrong(s1, s2):
-    #         if len(s1) != len(s2):
-    #             return False
-    #         char_count = {}
-    #         for char in s1:
-    #             char_count[char] = char_count.get(char, 0) + 1
-    #         for char in s2:
-    #             if char not in char_count or char_count[char] == 0:
-    #                 return False
-    #             char_count[char] -= 1
-    #         # 错误地在这里直接返回True
-    #         return True
-    #     hashtable = {}
-    #     sorted_strs = []
-    #     result = []
-    #     i = 0
-    #     for index, s in enumerate(strs):
-    #         sorted_strs.append("".join(sorted(s)))
-    #     for index, s in enumerate(sorted_strs):
-    #         if s not in hashtable:
-    #             result.append([strs[index]])
-    #             hashtable[s] = i
-    #             i += 1
-    #         else:
-    #             result[hashtable[s]].append(strs[index])
-    #     print(result)
-    #     return result
 # @lc code=end
 solution = Solution()
 solution.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
