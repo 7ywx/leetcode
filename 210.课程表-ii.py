7c4120392e07ef13029891cba2baffb17a104342
ywx-desktop <2819142473@qq.com>
2024-10-06 20:49:57 +0800

    。

---
+++ 210.课程表-ii.py
@@ -0,0 +1,105 @@
+#
+# @lc app=leetcode.cn id=210 lang=python3
+#
+# [210] 课程表 II
+#
+# https://leetcode.cn/problems/course-schedule-ii/description/
+#
+# algorithms
+# Medium (58.40%)
+# Likes:    973
+# Dislikes: 0
+# Total Accepted:    249.8K
+# Total Submissions: 427.5K
+# Testcase Example:  '2\n[[1,0]]'
+#
+# 现在你总共有 numCourses 门课需要选，记为 0 到 numCourses - 1。给你一个数组 prerequisites ，其中
+# prerequisites[i] = [ai, bi] ，表示在选修课程 ai 前 必须 先选修 bi 。
+#
+#
+# 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示：[0,1] 。
+#
+#
+# 返回你为了学完所有课程所安排的学习顺序。可能会有多个正确的顺序，你只要返回 任意一种 就可以了。如果不可能完成所有课程，返回 一个空数组 。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：numCourses = 2, prerequisites = [[1,0]]
+# 输出：[0,1]
+# 解释：总共有 2 门课程。要学习课程 1，你需要先完成课程 0。因此，正确的课程顺序为 [0,1] 。
+#
+#
+# 示例 2：
+#
+#
+# 输入：numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+# 输出：[0,2,1,3]
+# 解释：总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+# 因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
+#
+# 示例 3：
+#
+#
+# 输入：numCourses = 1, prerequisites = []
+# 输出：[0]
+#
+#
+#
+# 提示：
+#
+#
+# 1 <= numCourses <= 2000
+# 0 <= prerequisites.length <= numCourses * (numCourses - 1)
+# prerequisites[i].length == 2
+# 0 <= ai, bi < numCourses
+# ai != bi
+# 所有[ai, bi] 互不相同
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indegree = [0 for _ in range(numCourses)] # BFS
+        flags = [0 for _ in range(numCourses)] # DFS 0: 未访问, 1: 正在访问, -1: 已访问
+        adjacency = [[] for _ in range(numCourses)]
+        ans = []
+
+        for cur, pre in prerequisites:
+            adjacency[pre].append(cur) # pre -> cur
+            indegree[cur] += 1
+
+        indegreeZero = [i for i, x in enumerate(indegree) if x == 0]
+
+        while indegreeZero:
+            i = indegreeZero.pop()  # 当前入度为0的课程
+            ans.append(i)
+            for j in adjacency[i]:  # 获取当前课程的后继课程列表 (读完当前课程，后继课程入度-1)
+                indegree[j] -= 1  # 减少后继课程的入度
+                if indegree[j] == 0:  # 如果后继课程的入度也变为0，加入列表
+                    indegreeZero.append(j)
+
+        return ans if len(ans) == numCourses else []
+
+
+        def dfs(i):
+            if flags[i] == -1: return True
+            if flags[i] == 1: return False
+            flags[i] = 1
+            for j in adjacency[i]:
+
+                if flags[j] != -1: ans.append(j)
+                if not dfs(j): return False
+            flags[i] = -1
+            return True
+
+        # DFS
+        for i in range(numCourses):
+            ans.append(i)
+            if dfs(i) == False: return []
+            else: return ans
+# @lc code=end
