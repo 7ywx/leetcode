f3e3fe7b75b2c84559bdf09ead5f8f92bdfb5c99
7ywx <y28192473@163.com>
2025-01-09 10:32:22 +0800

    快排

--- 快速排序.py
+++ 快速排序.py
@@ -125,3 +125,67 @@ def quicksort(arr):
 # 示例
 arr = [3, 6, 8, 10, 1, 2, 1]
 print(quicksort(arr))
+
+# 常见
+def quick_sort(array, left, right):
+    if left >= right:
+        return
+    low = left
+    high = right
+    key = array[low]
+    while left < right:
+        # right找小于key的
+        while left < right and array[right] > key:
+            right -= 1
+        array[left] = array[right] # 小于key放left
+
+        # left找大于key的
+        while left < right and array[left] <= key:
+            left += 1
+        array[right] = array[left] # 大于key放right
+
+    array[right] = key # 确定key的位置
+    quick_sort(array, low, left - 1)
+    quick_sort(array, left + 1, high)
+
+# 算法导论
+def quick_sort(array, l, r):
+    if l < r:
+        q = partition(array, l, r)
+        quick_sort(array, l, q - 1)
+        quick_sort(array, q + 1, r)
+
+def partition(array, l, r):
+    x = array[r]
+    i = l - 1
+    for j in range(l, r):
+        if array[j] <= x:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[r] = array[r], array[i+1]
+    return i + 1
+
+# 用栈实现的
+def quick_sort(array, l, r):
+    if l >= r:
+        return
+    stack = []
+    stack.append(l)
+    stack.append(r)
+    while stack:
+        low = stack.pop(0)
+        high = stack.pop(0)
+        if high - low <= 0:
+            continue
+        x = array[high]
+        i = low - 1
+        for j in range(low, high):
+            if array[j] <= x:
+                i += 1
+                array[i], array[j] = array[j], array[i]
+        array[i + 1], array[high] = array[high], array[i + 1]
+        stack.extend([low, i, i + 2, high])
+
+s = [4,5,3,7,2,2,3]
+quick_sort(s,0,6)
+print(s)
