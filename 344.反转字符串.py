49dbf83a3284d441b5622d55c9cac4f4af746c0b
workplace <2819142473@qq.com>
2025-03-19 16:49:33 +0800

    添加了双指针法，太简单了没必要写注释

--- 344.反转字符串.py
+++ 344.反转字符串.py
@@ -48,8 +48,14 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s[:] = s[::-1]
-        return s
+        # 双指针
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+        # s[:] = s[::-1]
 # @lc code=end
 
 
