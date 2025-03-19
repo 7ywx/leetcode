bee6d66fad5849fdd9571012e554142cf8b697ff
workplace <2819142473@qq.com>
2025-03-19 11:41:39 +0800

    注释

--- 541.反转字符串 II.py
+++ 541.反转字符串 II.py
@@ -50,15 +50,27 @@
 # @lc code=start
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
+        # 将输入的字符串转换为列表，以便进行就地修改
         s = list(s)
+        # 计算字符串的长度
         n = len(s)
+        # 计算可以进行完整反转的次数
         times = n//(2*k)
+
+        # 对于每个完整的反转周期
         for i in range(times):
+            # 反转当前周期内的子字符串
             s[i*2*k : (i*2+1)*k] = s[i*2*k : (i*2+1)*k][::-1]
+
+        # 处理剩余的部分，如果剩余长度大于k
         if n % (2*k) > k:
+            # 只反转剩余部分的前k个字符
             s[times*2*k : (times*2+1)*k] = s[times*2*k : (times*2+1)*k][::-1]
         else:
+            # 如果剩余长度不大于k，反转所有剩余的字符
             s[times*2*k :] = s[times*2*k : ][::-1]
+
+        # 最后，将修改后的字符列表重新连接成字符串并返回
         return "".join(s)
 
 # @lc code=end
